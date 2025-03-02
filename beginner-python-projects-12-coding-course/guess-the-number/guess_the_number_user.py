88e8276a54fda717cf5f238a1c7cfe4d22b5a7ed
MERVECETIN1929 <mervecetinnitec@gmail.com>
2025-03-02 12:26:46 +0300

    Add number guessing game where computer makes guesses

---
+++ beginner-python-projects-12-coding-course/guess-the-number/guess_the_number_user.py
@@ -0,0 +1,23 @@
+import random
+
+
+def random_number_generator(random_number_start_point, random_number_end_point):
+    return random.randrange(random_number_start_point, random_number_end_point)
+
+
+random_number_start_point = 0
+random_number_end_point = 100
+
+while True:
+    random_number = random_number_generator(random_number_start_point,
+                                            random_number_end_point)
+    user_info = input(
+        f"My guess is {random_number}. Please give me info. (L:low , H:high, C: correct)").lower()
+    if user_info == "c":
+        print("Nice game")
+        break
+    elif user_info == "h":
+        random_number_end_point = random_number+1
+
+    elif user_info == "l":
+        random_number_start_point = random_number-1
